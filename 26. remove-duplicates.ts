da60c880faf7a1cddb2c34e9ed0ebc45e54a737b
ostsam <ost.sam@gmail.com>
2025-06-26 10:55:49 -0400

    26. remove duplicates

---
+++ 26. remove-duplicates.ts
@@ -0,0 +1,11 @@
+const arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+
+function removeDuplicates(nums: number[]): number {
+  for (let i = nums.length; i >= 0; i--) {
+    if (i > 0 && nums[i] == nums[i - 1]) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums.length;
+}
+const noDups = removeDuplicates(arr)
